b474e9f1eda953c9dec7ed119b3652898f6591a8
João Pedro <irus0ubusiness@gmail.com>
2024-05-10 11:43:57 +0100

    feat: filter games by platform

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,5 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "./../hooks/useGames";
+import useGames, { Platform } from "./../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
@@ -7,10 +7,14 @@ import { Genre } from "../hooks/useGenres";
 
 interface GameGridProps {
 	selectedGenre: Genre | null;
+	selectedPlatform: Platform | null;
 }
 
-export default function GameGrid({ selectedGenre }: GameGridProps) {
-	const { data, error, isLoading } = useGames(selectedGenre);
+export default function GameGrid({
+	selectedGenre,
+	selectedPlatform,
+}: GameGridProps) {
+	const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
 	const skeletons = [1, 2, 3, 4, 5, 6];
 
 	return (
