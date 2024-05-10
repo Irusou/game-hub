81c18895c5da8492b44bc90dad8be63a283f0cf1
João Pedro <irus0ubusiness@gmail.com>
2024-05-10 12:35:38 +0100

    feat: searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,15 @@ import logo from "../assets/orange-cat.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { SearchInput } from "./SearchInput";
 
-interface NavBarProps {}
-export default function NavBar(props: NavBarProps) {
+interface NavBarProps {
+	onSearch: (searchText: string) => void;
+}
+export default function NavBar({ onSearch }: NavBarProps) {
 	return (
 		<>
 			<HStack padding="10px">
 				<Image src={logo} boxSize="60px" borderRadius="8px" />
-				<SearchInput />
+				<SearchInput onSearch={onSearch} />
 				<ColorModeSwitch />
 			</HStack>
 		</>
