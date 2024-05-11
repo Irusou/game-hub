e54655155ce13ea37d5f19ab916c216251520d72
João Pedro <irus0ubusiness@gmail.com>
2024-05-11 18:02:26 +0100

    alt: cleanup game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -21,10 +21,10 @@ export default function GameGrid({ gameQuery }: GameGridProps) {
 					sm: 1,
 					md: 2,
 					lg: 3,
-					xl: 5,
+					xl: 4,
 				}}
 				padding="10px"
-				spacing={3}
+				spacing={6}
 			>
 				{isLoading &&
 					skeletons.map((skeleton) => (
